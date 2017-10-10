6327239d3fe0864854a889c5afd5570e94d59748
skyream <794912375@qq.com>
2017-10-10 16:28:56 +0800

    unity opencv pc
    录制视频

---
+++ UnityOpenCVMp4/Assets/Record.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+public class Record
+{
+    [DllImport("OpencvVideoWriter")]
+    private static extern bool MPEGInitData(byte[] path, int fps,int width, int height);
+    [DllImport("OpencvVideoWriter")]
+    private static extern void MPEGData(byte[] imageData);
+    [DllImport("OpencvVideoWriter")]
+    private static extern void MPEGRelease();
+
+    /// <summary>
+    /// 录制初始化
+    /// </summary>
+    public static bool MPEGInitDataToC(string path, int fps, int width, int height)
+    {
+        byte[] nameArr = System.Text.Encoding.Default.GetBytes(path);
+        bool isOk = MPEGInitData(nameArr,fps, width, height);
+        return isOk;
+    }
+    /// <summary>
+    /// 更新视频
+    /// </summary>
+    /// <param name="bytes"></param>
+    public static void MPEGDataToC(byte[] bytes)
+    {
+        MPEGData(bytes);
+    }
+    /// <summary>
+    /// 录制完成
+    /// </summary>
+    public static void MPEGReleaseToC()
+    {
+        MPEGRelease();
+    }
+}
---
+++ UnityOpenCVMp4/Assets/WebCamManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class WebCamManager : MonoBehaviour
+{
+    private  int index;
+    private WebCamTexture webcamTexture;
+    private Texture2D texture2D;
+    private bool isOk;
+    void Start()
+    {
+         webcamTexture = new WebCamTexture();
+        if (WebCamTexture.devices.Length > 0)
+        {
+            webcamTexture.deviceName = WebCamTexture.devices[0].name;
+            webcamTexture.Play();
+            int width = webcamTexture.width;
+            int height = webcamTexture.height;
+            isOk = Record.MPEGInitDataToC("Test.mp4", 24, webcamTexture.width, webcamTexture.height);
+            Debug.Log("初始化:" + isOk);
+            texture2D = new Texture2D(width, height, TextureFormat.RGB24, false);
+        }
+    }
+    private byte[] ByteByColors(Color[] colors)
+    {
+        byte[] bytes = new byte[colors.Length * 3];
+        for (int i = 0; i < colors.Length; i++)
+        {
+            bytes[i * 3 + 0] = (byte)(colors[i].r * 255);
+            bytes[i * 3 + 1] = (byte)(colors[i].g * 255);
+            bytes[i * 3 + 2] = (byte)(colors[i].b * 255);
+        }
+        return bytes;
+    }
+    private void Update()
+    {
+        if (isOk)
+        {
+            int width = webcamTexture.width;
+            int height = webcamTexture.height;
+            texture2D.SetPixels(webcamTexture.GetPixels());
+            texture2D.Apply();
+            index++;
+            if (index < 300)
+            {
+                byte[] arr = texture2D.GetRawTextureData();// ByteByColors(webcamTexture.GetPixels());
+                Record.MPEGDataToC(arr);
+            }
+
+            else if (index == 300)
+            {
+                Record.MPEGReleaseToC();
+            }
+
+        }
+    }
+}
